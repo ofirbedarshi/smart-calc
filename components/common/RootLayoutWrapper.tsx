ea438be8e29a6992dc0ac98bf56875f41dbba82f
Ofir Bedarshi <ofir.bedarshi@myobligo.com>
2025-06-24 08:57:33 +0300

    Force LTR globally

--- components/common/RootLayoutWrapper.tsx
+++ components/common/RootLayoutWrapper.tsx
@@ -1,8 +1,10 @@
 import { useFonts } from 'expo-font';
 import { SplashScreen } from 'expo-router';
 import React, { useEffect } from 'react';
+import { I18nManager } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
 
 interface RootLayoutWrapperProps {
   children: React.ReactNode;
@@ -12,6 +14,13 @@ const RootLayoutWrapper: React.FC<RootLayoutWrapperProps> = ({ children }) => {
   const [loaded] = useFonts({
     SpaceMono: require('../../assets/fonts/SpaceMono-Regular.ttf'),
   });
+
+  // Force LTR globally
+  React.useEffect(() => {
+    if (!I18nManager.isRTL) return;
+    I18nManager.forceRTL(false);
+    I18nManager.allowRTL(false);
+  }, []);
 
   useEffect(() => {
     if (loaded) {

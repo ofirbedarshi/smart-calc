def25a8c534906c348723b48224ceb3ae6dc7ea8
Ofir Bedarshi <ofirbed@gmail.com>
2025-07-03 09:28:44 +0300

    Location view refactor

--- components/location/LocationView.tsx
+++ components/location/LocationView.tsx
@@ -17,32 +17,61 @@ export const LocationView: React.FC<LocationViewProps> = ({ locationData }) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.locationText}>
-        <Text style={styles.identifier}>גו'</Text>
-        <Text style={styles.value}>{locationData.height}</Text>
-        <Text style={styles.identifier}>/צפ'</Text>
-        <Text style={styles.value}>{locationData.northCoord}</Text>
-        <Text style={styles.identifier}>/מז'</Text>
-        <Text style={styles.value}>{locationData.eastCoord}</Text>
-      </Text>
+      <View style={styles.row}>
+      <View style={styles.coordBox}>
+          <Text style={styles.identifier}>גו'</Text>
+          <Text style={styles.value}>{locationData.height}</Text>
+        </View>
+        <View style={styles.coordBox}>
+          <Text style={styles.identifier}>צפ'</Text>
+          <Text style={styles.value}>{locationData.northCoord}</Text>
+        </View>
+       
+        <View style={styles.coordBox}>
+          <Text style={styles.identifier}>מז'</Text>
+          <Text style={styles.value}>{locationData.eastCoord}</Text>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 8,
+    padding: 0,
   },
-  locationText: {
-    fontSize: 16,
-    textAlign: 'right',
+  row: {
+    flexDirection: 'row-reverse',
+    justifyContent: 'flex-start',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    marginTop: 4,
+    rowGap: '10px'
+  },
+  coordBox: {
+    backgroundColor: '#e6e8e5',
+    borderColor: '#222',
+    borderWidth: 1,
+    borderRadius: 5,
+    minWidth: 80,
+    minHeight: 32,
+    marginHorizontal: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+    flexDirection: 'row-reverse',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   identifier: {
-    color: '#666',
+    color: '#444',
+    fontSize: 15,
+    marginLeft: 6,
   },
   value: {
-    color: '#000',
+    color: '#222',
     fontWeight: 'bold',
+    fontSize: 15,
+    marginLeft: 4,
   },
   unknownText: {
     fontSize: 16,

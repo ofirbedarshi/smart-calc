174ad64ee3cd324bfd90aa9208eb7d30b9e26b27
Ofir Bedarshi <ofirbed@gmail.com>
2025-07-08 09:17:15 +0300

    krav lazel loal

--- components/nadbars/krav/kravLaserLoalTemplate.ts
+++ components/nadbars/krav/kravLaserLoalTemplate.ts
@@ -8,12 +8,40 @@ export const DEFAULT_KRAV_LASER_LOAL_TEMPLATE: NadbarTemplate = {
     {
       type: 'form',
       data: [
-        { label: 'שדה דוגמה', fieldId: 'exampleField', inputType: 'text' },
-      ],
+        { label: 'תדר', fieldId: 'freq', inputType: 'text' },
+        { label: 'או״ק חוליה', fieldId: 'ok_team', inputType: 'text' },
+        { label: 'אזימוט', fieldId: 'azimuth', inputType: 'text', targetField: 'azimuth' },
+        { label: 'טווח', fieldId: 'distance', inputType: 'text', targetField: 'distance' }
+      ]
+    },
+    {
+      type: 'form',
+      data: [
+        { label: 'שם ותיאור מטרה', fieldId: 'targetName', inputType: 'text', targetField: 'description' },
+        { label: 'נ.צ GEO צפוני', fieldId: 'con_geoNorth', inputType: 'text' },
+        { label: 'נ.צ GEO מזרחי', fieldId: 'con_geoEast', inputType: 'text' },
+        { label: 'גובה מטרה - רגל', fieldId: 'heightRegel'},
+        { label: 'כיוון כניסה מומלץ', fieldId: 'enterence'},
+        { label: 'קסום', fieldId: 'kasoom', inputType: 'text' },
+        { label: 'עננות', fieldId: 'clouds', inputType: 'text' },
+        { label: 'גובה עננים', fieldId: 'cloudsHeight', inputType: 'text' },
+        { label: 'נק מכוון', fieldId: 'point', inputType: 'text' },
+        { label: 'סוג מטרה', fieldId: 'targetType', inputType: 'dropdown', 
+          inputOptions: { dropdown: ['קיר', 'גג', 'שטח'] }  }
+      ]
+    },
+    {
+      type: 'header',
+      data: 'חוזר על הנתונים לאימות',
     },
     {
       type: 'text',
-      data: 'טקסט דוגמה לקרב ברד לייזר loal',
+      header: 'יעף לבן',
+      data: `יציבות נעילה - תקינות ציון, תנאים מאפשרים, מדידת זמן יעף +הטלה<br> סידור רכיבים במרחב המטרה - קבלת אימות אחרון`
+    },
+    {
+      type: 'text',
+      data: `קבל יוצא כיוון ליעף ברד לייזר על __ זמן מעוף פצצה שוער __ (נווט)<br>קבל הפעל קסום (נווט)<br><b>שדר מופעל קסום על __רשאי להטיל ברד לייזר (מטרה בתנועה - שדר נ״צ_גובה של נקודת פגיעה צפויה)</b><br>קבל דקה להטלה (טייס)<br>קבל אישור להטלה (נווט)<br>קבל הוטל __ שניות<br>קבל ספירת שניות לאחור (טייס)<br><b>שדר BDA</b>`
     },
   ],
 }; 

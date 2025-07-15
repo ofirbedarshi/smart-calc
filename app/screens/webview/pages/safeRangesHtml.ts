00e258d078aa3a11141d86eb4940782ea09f6e98
Ofir Bedarshi <ofirbed@gmail.com>
2025-07-15 14:45:04 +0300

    update safe range html

--- app/screens/webview/pages/safeRangesHtml.ts
+++ app/screens/webview/pages/safeRangesHtml.ts
@@ -1,121 +1,55 @@
 const safeRangesHtml = `
-<table style="width: 72.5253%; border-collapse: collapse; border-color: #000000; border-style: solid;" border="1"><colgroup><col style="width: 21.7877%;" width="78"><col style="width: 25.6983%;" width="92"><col style="width: 33.5196%;" width="120"><col style="width: 18.9944%;" width="68"></colgroup>
+<table style="width: 94.5716%; border-collapse: collapse; border-color: #000000; border-style: solid; height: 914.547px;" border="1"><colgroup><col style="width: 21.8627%;" width="78"><col style="width: 25.6571%;" width="92"><col style="width: 33.6072%;" width="120"><col style="width: 18.9718%;" width="68"></colgroup>
 <tbody>
-<tr style="background-color: #c2e0f4;">
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">גורם אש</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">חימוש</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">כוחותינו</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">בלתי מעורבים</p>
-</td>
+<tr style="background-color: #bfedd2; text-align: center; height: 68px;">
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">גורם אש</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">חימוש</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">כוחותינו</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">בלתי מעורבים</span></td>
 </tr>
-<tr>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">ארטילריה</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">נפיץ/ תאורה/ עשן</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">טיווח: 1000 מ</p>
-<p dir="rtl">תכלית גלויים: 350 מ</p>
-<p dir="rtl">תכלית משוריינים: 250 מ</p>
-</td>
-<td dir="ltr" style="border-color: #000000;"><p dir="rtl">330 מ</p></td>
+<tr style="text-align: center; height: 144px;">
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">ארטילריה</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">נפיץ/ תאורה/ עשן</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">טיווח: 1000 מ</span> <span dir="rtl">תכלית גלויים: 350 מ</span> <span dir="rtl">תכלית משוריינים: 250 מ</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">330 מ</span></td>
 </tr>
-<tr>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">מרגמות</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">נפיץ/ תאורה/ עשן</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">טיווח: 500 מ</p>
-<p dir="rtl">תכלית יום: 300 מ</p>
-<p dir="rtl">תכלית לילה: 300 מ</p>
-</td>
-<td dir="ltr" style="border-color: #000000;"><p dir="rtl">300 מ</p></td>
+<tr style="text-align: center; height: 144px;">
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">מרגמות</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">נפיץ/ תאורה/ עשן</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">טיווח: 500 מ</span> <span dir="rtl">תכלית יום: 300 מ</span> <span dir="rtl">תכלית לילה: 300 מ</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">300 מ</span></td>
 </tr>
-<tr>
-<td dir="ltr" style="border-color: #000000;" rowspan="2"><br><br><br>
-<p dir="rtl">חממ</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">תמוז</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">נט: 250 יום, 500 לילה</p>
-<p dir="rtl">נא: 500 יום, 1000 לילה</p>
-</td>
-<td dir="ltr" style="border-color: #000000;" rowspan="2"><br><br><br>
-<p dir="rtl">100 מ</p>
-</td>
+<tr style="text-align: center; height: 106px;">
+<td dir="ltr" style="border-color: #000000;" rowspan="2"><span dir="rtl">חממ</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">תמוז</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">נט: 250 יום, 500 לילה</span> <span dir="rtl">נא: 500 יום, 1000 לילה</span></td>
+<td dir="ltr" style="border-color: #000000;" rowspan="2"><br><br><br><span dir="rtl">100 מ</span></td>
 </tr>
-<tr>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">גיל, להט, עוקץ פלדה</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">כוח גלוי: 100 מ</p>
-<p dir="rtl">כוח משוריין/ במבנה: 100 מ</p>
-</td>
+<tr style="text-align: center; height: 106px;">
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">גיל, להט, עוקץ פלדה</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">כוח גלוי: 100 מ</span> <span dir="rtl">כוח משוריין/ במבנה: 100 מ</span></td>
 </tr>
-<tr>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">מסק״ר</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">קרדום, פגיון, תמוז, גיל</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">ישיר: 100 מ טיל מדויק, 300 מ תותח</p>
-<p dir="rtl">עקיף: 300 מ</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">נט: 5 מ</p>
-<p dir="rtl">רסס: 15 מ</p>
-</td>
+<tr style="text-align: center; height: 128px;">
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">מסק״ר</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">קרדום, פגיון, תמוז, גיל</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">ישיר: 100 מ טיל מדויק, 300 מ תותח</span> <span dir="rtl">עקיף: 300 מ</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">נט: 5 מ</span> <span dir="rtl">רסס: 15 מ</span></td>
 </tr>
-<tr>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">זיק</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">מכחולית</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">כוח נייח: 100 מ</p>
-<p dir="rtl">כוח נייד: 300 מ</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">נט: 5 מ</p>
-<p dir="rtl">רסס: 15 מ</p>
-</td>
+<tr style="text-align: center; height: 106px;">
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">זיק</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">מכחולית</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">כוח נייח: 100 מ</span> <span dir="rtl">כוח נייד: 300 מ</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">נט: 5 מ</span> <span dir="rtl">רסס: 15 מ</span></td>
 </tr>
-<tr>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">קרב</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">כלל סוגי החימושים</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">שטח בנוי: 250 מ</p>
-<p dir="rtl">שטח פתוח: 500 מ</p>
-</td>
-<td dir="ltr" style="border-color: #000000;">
-<p dir="rtl">לפי נז״א</p>
-</td>
+<tr style="text-align: center; height: 112px;">
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">קרב</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">כלל סוגי החימושים</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">שטח בנוי: 250 מ</span> <span dir="rtl">שטח פתוח: 500 מ</span></td>
+<td dir="ltr" style="border-color: #000000;"><span dir="rtl">לפי נז״א</span></td>
 </tr>
 </tbody>
 </table>
-<p>&nbsp;</p>`;
+<p>&nbsp;</p>
+`;
 
 export { safeRangesHtml };
